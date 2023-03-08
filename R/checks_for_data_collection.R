b1de09684c5c60c9a37842b30479caa61bb64c09
Anthony Twesigye <twesigye10@gmail.com>
2023-03-08 12:00:23 +0300

    other_specify

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(lubridate)
 library(glue)
+library(supporteR)
 
 # read data ---------------------------------------------------------------
 
@@ -10,7 +11,10 @@ df_tool_data <- readxl::read_excel("inputs/ETH2002_H2R_data.xlsx") |>
          i.check.start_date = as_date(start),
          i.check.enumerator_id = enumerator_id,
          i.check.district_name = ifelse(is.na(loc_zone), loc_zone_other, loc_zone),
+         i.check.loc_zone = ifelse(is.na(loc_zone), loc_zone_other, loc_zone),
+         int.loc_zone = ifelse(is.na(loc_zone), loc_zone_other, loc_zone),
          i.check.point_number = "",
+         point_number = "",
          start = as_datetime(start),
          end = as_datetime(end))
 
@@ -20,3 +24,25 @@ loc_tool <- "inputs/ETH2002_H2R_tool.xlsx"
 
 df_survey <- readxl::read_excel(loc_tool, sheet = "survey")
 df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
+
+
+# checks ------------------------------------------------------------------
+
+checks_output <- list()
+
+# check duplicate uuids
+
+# outliers
+
+
+# other_specify
+df_others_data <- extract_other_specify_data(input_tool_data = df_tool_data, 
+                                             input_point_id_col = "point_number", 
+                                             input_location_col = "int.loc_zone",
+                                             input_survey = df_survey,  
+                                             input_choices = df_choices)
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_others_data")
+
+
+# logical checks
