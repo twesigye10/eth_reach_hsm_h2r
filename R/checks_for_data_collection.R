01c253b200880b5cfa3908aabbd78f2eb8da5c8c
Anthony Twesigye <twesigye10@gmail.com>
2023-03-13 09:21:57 +0300

    logic_c_time_short_but_school_far_16

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -413,6 +413,29 @@ df_logic_c_time_short_but_health_facility_far_15 <- df_tool_data |>
 
 add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_logic_c_time_short_but_health_facility_far_15")
 
+# logic_c_time_short_but_school_far_16
+df_logic_c_time_short_but_school_far_16 <- df_tool_data |> 
+  filter(time_to_closest_functional_primary_school %in% c("under_30_minutes"),
+         str_detect(string = main_barrier_accessing_education_girls, pattern = "distance_to_school_too_far")) |> 
+  mutate(i.check.type = "change_response",
+         i.check.name = "time_to_closest_functional_primary_school",
+         i.check.current_value = time_to_closest_functional_primary_school,
+         i.check.value = "",
+         i.check.issue_id = "logic_c_time_short_but_school_far_16",
+         i.check.issue = glue("time_to_closest_functional_primary_school: {time_to_closest_functional_primary_school} but main_barrier_accessing_education_girls: {main_barrier_accessing_education_girls}"),
+         i.check.other_text = "",
+         i.check.checked_by = "",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "")  |> 
+  batch_select_rename()
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "logic_c_time_short_but_school_far_16")
+
+
+
 
 
 
