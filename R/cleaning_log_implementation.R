c9005394cbc9812b678419c7353c1058af97d741
Anthony Twesigye <twesigye10@gmail.com>
2023-03-29 09:33:45 +0300

    updating pipe

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -4,20 +4,20 @@ library(lubridate)
 library(glue)
 library(supporteR)
 
-# Read data and checking log
+# Read data and checking log 
 
-df_cleaning_log <- read_csv("inputs/combined_checks_h2r_eth.csv", col_types = cols(sheet = "c", index = "i")) %>% 
-  filter(!adjust_log %in% c("delete_log")) %>%
+df_cleaning_log <- read_csv("inputs/combined_checks_h2r_eth.csv", col_types = cols(sheet = "c", index = "i")) |> 
+  filter(!adjust_log %in% c("delete_log"), reviewed %in% c("1")) |>
   mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log),
          value = ifelse(is.na(value) & str_detect(string = issue_id, pattern = "logic_c_"), "blank", value),
          value = ifelse(type %in% c("remove_survey"), "blank", value),
          name = ifelse(is.na(name) & type %in% c("remove_survey"), "point_number", name)
-  ) %>% 
-  filter(!is.na(value), !is.na(uuid)) %>%
+  ) |> 
+  filter(!is.na(value), !is.na(uuid)) |>
   mutate(value = ifelse(value %in% c("blank"), NA, value),
          sheet = NA,
          index = NA,
-         relevant = NA) %>%
+         relevant = NA) |>
   select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
 
 # raw data
@@ -30,7 +30,7 @@ cols_to_escape <- c("index", "start", "end", "today", "starttime",	"endtime", "_
 data_nms <- names(readxl::read_excel(path = loc_data, n_max = 2000))
 c_types <- ifelse(str_detect(string = data_nms, pattern = "_other$"), "text", "guess")
 
-df_raw_data <- readxl::read_excel(path = loc_data, col_types = c_types) %>% 
+df_raw_data <- readxl::read_excel(path = loc_data, col_types = c_types) |> 
   mutate(across(.cols = -c(contains(cols_to_escape)), 
                 .fns = ~ifelse(str_detect(string = ., 
                                           pattern = fixed(pattern = "N/A", ignore_case = TRUE)), "NA", .)))
@@ -39,18 +39,19 @@ df_raw_data <- readxl::read_excel(path = loc_data, col_types = c_types) %>%
 loc_tool <- "inputs/ETH2002_H2R_tool.xlsx"
 
 df_survey <- readxl::read_excel(loc_tool, sheet = "survey")
-df_choices <- readxl::read_excel(loc_tool, sheet = "choices")
+df_choices <- readxl::read_excel(loc_tool, sheet = "choices") |> 
+  mutate(label = `label::english`)
 
 
 # main dataset ------------------------------------------------------------
 
-df_cleaning_log_main <-  df_cleaning_log %>% 
+df_cleaning_log_main <-  df_cleaning_log |> 
   filter(is.na(sheet))
 
 df_cleaned_data <- supporteR::cleaning_support(input_df_raw_data = df_raw_data,
                                               input_df_survey = df_survey,
                                               input_df_choices = df_choices,
-                                              input_df_cleaning_log = df_cleaning_log_main) %>% 
+                                              input_df_cleaning_log = df_cleaning_log_main) |> 
   mutate(across(.cols = -c(any_of(cols_to_escape), matches("_age$|^age_|uuid")),
                 .fns = ~ifelse(str_detect(string = ., pattern = "^[9]{2,9}$"), "NA", .)))
 
