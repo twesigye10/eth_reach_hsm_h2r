cfb8fa7be4528b7ddea0151523f827c116a6067a
Anthony Twesigye <twesigye10@gmail.com>
2023-04-13 12:00:44 +0300

    delete_log: logic_c_no_population_remained_3

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -147,8 +147,8 @@ df_logic_c_no_population_remained_3 <- df_tool_data |>
          i.check.checked_by = "",
          i.check.checked_date = as_date(today()),
          i.check.comment = "", 
-         i.check.reviewed = "",
-         i.check.adjust_log = "",
+         i.check.reviewed = "1",
+         i.check.adjust_log = "delete_log",
          i.check.so_sm_choices = "")  |> 
   batch_select_rename()
 
