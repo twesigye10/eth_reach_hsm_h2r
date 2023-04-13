29a80db390714f901d4d1c4e4dcc63f6b9d466af
Anthony Twesigye <twesigye10@gmail.com>
2023-04-13 14:19:20 +0300

    logic_c_access_sanitation_but_open_defecation_23

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -575,8 +575,8 @@ df_logic_c_access_sanitation_but_open_defecation_23 <- df_tool_data |>
          i.check.checked_by = "",
          i.check.checked_date = as_date(today()),
          i.check.comment = "", 
-         i.check.reviewed = "",
-         i.check.adjust_log = "",
+         i.check.reviewed = "1",
+         i.check.adjust_log = "delete_log",
          i.check.so_sm_choices = "")  |> 
   batch_select_rename()
 
